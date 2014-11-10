e3b7ff376c9d338b9523be2585894ab3df72c5e0
Alexey Emelyanov <emelyanoff@gmail.com>
2014-11-10 14:04:40 -0800

    start

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -29,7 +29,7 @@
 
 - (void)performSearchWithKeyword:(NSString *)keyword
 {
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.meetup.com/2/open_events.json?zip=60604&text=%@&time=,1w&key=4b6a576833454113112e241936657e47",keyword]];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.meetup.com/2/open_events.json?zip=60604&text=%@&time=,1w&key=3c7f626d333e3a7433a44552f6b775f",keyword]];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
